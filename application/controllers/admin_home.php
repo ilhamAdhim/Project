4377e7e8b0db81f0885955137359563a3a6990e6
M Ilham Adhim <43105014+ilhamAdhim@users.noreply.github.com>
2020-03-30 09:59:02 +0700

    Add session

--- application/controllers/admin_home.php
+++ application/controllers/admin_home.php
@@ -33,40 +33,89 @@ class admin_home extends CI_Controller {
     }
 
     // CREATE
-        // public function createResearchGroup(){
-        //     $this->form_validation->set_rules('nama','Nama','required');
-        //     $this->form_validation->set_rules('nim','Nim','required|numeric');
-        //     $this->form_validation->set_rules('email','Email','required|valid_email');
+        public function createResearchGroup(){
+            if($this->session->userdata('loggedIn')){
+                $this->admin_model->createResearchGroups();
+                redirect('admin_home/researchGroup');
+            }else{
+                redirect(base_url());
+            }
+        }
 
-        //     if($this->form_validation->run()){
-        //         $this->mahasiswa_model->tambahdatamhs();
-        //         $this->session->set_flashdata('flash-data', 'ditambahkan');
-        //         redirect('mahasiswa','refresh');
+        public function createClasses(){
+            if($this->session->userdata('loggedIn')){
+                $this->admin_model->createClass();
+                redirect('admin_home/classList');
+            }else{
+                redirect(base_url());
+            }
+        }
+
+        public function createSubjects(){
+            if($this->session->userdata('loggedIn')){
+                $this->admin_model->createSubject();
+                redirect('admin_home/subjectList');
+            }else{
+                redirect(base_url());
+            }
+        }
+        /* public function createStatusLecturer(){
+            
+        } */
+        /* public function createFieldLecturer(){
+            
+        } */
+        public function createPositionLecturer(){
+            if($this->session->userdata('loggedIn')){
                 
-        //     }else{
-        //         $this->load->view('template/header',$data);
-        //         $this->load->view('mahasiswa/tambah',$data);
-        //         $this->load->view('template/footer');
-        //     }
-        // }
+            }else{
+                redirect(base_url());
+            }
+        }
+        public function createDPALecturer(){
+            if($this->session->userdata('loggedIn')){
+                
+            }else{
+                redirect(base_url());
+            }
+        }
+        public function createResearchGroupLecturer(){
+            
+            if($this->session->userdata('loggedIn')){
+                
+            }else{
+                redirect(base_url());
+            }
+        }
+        public function createHourDist(){
+            
+            if($this->session->userdata('loggedIn')){
+                
+            }else{
+                redirect(base_url());
+            }
+        }
+        
 
     // READ
 
         public function researchGroup(){
             
-            //tb_research_category , tb_research_sub_category
-            $researchGroup = [
-                'data' => $this->admin_model->getResearchGroups(),
-                'title' => "Research Group",
-            ];
+            if($this->session->userdata('loggedIn')){
+                //tb_research_category , tb_research_sub_category
+                $researchGroup = [
+                    'data' => $this->admin_model->getResearchGroups(),
+                    'title' => "Research Group",
+                ];
 
-            /* $result =  $this->curl->simple_get($this->API.'?code='.$this->session->userdata('code'));
-            var_dump($result);
-            $data['response'] = json_decode($result, true); */
 
-            $this->load->view('template/header_admin', $researchGroup);
-            $this->load->view('home/admins/content', $researchGroup);
-            $this->load->view('template/footer_admin', $researchGroup);
+                $this->load->view('template/header_admin', $researchGroup);
+                $this->load->view('home/admins/content', $researchGroup);
+                $this->load->view('template/footer_admin', $researchGroup);
+            }else{
+                redirect(base_url());
+            }
+            
         }
 
 
@@ -74,92 +123,274 @@ class admin_home extends CI_Controller {
         public function classList(){
             //tb_class
 
-            $classes['data'] = $this->admin_model->getClasses();
-            $classes['title'] = 'Classes';        
-            $this->load->view('template/header_admin', $classes);
-            $this->load->view('home/admins/content', $classes);
-            $this->load->view('template/footer_admin', $classes);
+            if($this->session->userdata('loggedIn')){
+                $classes['data'] = $this->admin_model->getClasses();
+                $classes['title'] = 'Classes';        
+                $this->load->view('template/header_admin', $classes);
+                $this->load->view('home/admins/content', $classes);
+                $this->load->view('template/footer_admin', $classes);
+            }else{
+                redirect(base_url());
+            }
+            
         }
         
         public function subjectList(){
             // tb_subject
 
-            $subjects['data'] = $this->admin_model->getSubjects();
-            $subjects['title'] = 'Subjects';
+            if($this->session->userdata('loggedIn')){
+                $subjects['data'] = $this->admin_model->getSubjects();
+                $subjects['title'] = 'Subjects';
 
-            $this->load->view('template/header_admin', $subjects);
-            $this->load->view('home/admins/content', $subjects);
-            $this->load->view('template/footer_admin', $subjects);
+                $this->load->view('template/header_admin', $subjects);
+                $this->load->view('home/admins/content', $subjects);
+                $this->load->view('template/footer_admin', $subjects);
+                }else{
+                    redirect(base_url());
+                }
+            
         }
 
         // Lecturers
             public function statusLecturer(){
-            // vu_lecturer_status
-                $lcStatus['data'] = $this->admin_model->getLecturerStatus();
-                $lcStatus['title'] = 'Status Lecturer';
-        
-                $this->load->view('template/header_admin', $lcStatus);
-                $this->load->view('home/admins/content', $lcStatus);
-                $this->load->view('template/footer_admin', $lcStatus);
+                if($this->session->userdata('loggedIn')){
+                    // vu_lecturer_status
+                    $lcStatus['data'] = $this->admin_model->getLecturerStatus();
+                    $lcStatus['title'] = 'Status Lecturer';
+            
+                    $this->load->view('template/header_admin', $lcStatus);
+                    $this->load->view('home/admins/content', $lcStatus);
+                    $this->load->view('template/footer_admin', $lcStatus);
+                }else{
+                    redirect(base_url());
+                }
+            
             }
             
 
             public function fieldLecturer(){
-                // vu_lecturer_field 
+                if($this->session->userdata('loggedIn')){
+                    // vu_lecturer_field 
 
-                $lcField['data'] = $this->admin_model->getLecturerField();
-                $lcField['title'] = 'Field Lecturer';
+                    $lcField['data'] = $this->admin_model->getLecturerField();
+                    $lcField['title'] = 'Field Lecturer';
 
-                $this->load->view('template/header_admin', $lcField);
-                $this->load->view('home/admins/content', $lcField);
-                $this->load->view('template/footer_admin', $lcField);
+                    $this->load->view('template/header_admin', $lcField);
+                    $this->load->view('home/admins/content', $lcField);
+                    $this->load->view('template/footer_admin', $lcField);
+                }else{
+                    redirect(base_url());
+                }
+                
             }
 
             public function positionLecturer(){
+                if($this->session->userdata('loggedIn')){
                 //vu_position
+                    $lcPosition['data'] = $this->admin_model->getLecturerPosition();
+                    $lcPosition['title'] = 'Position Lecturer';
+    
+                    $this->load->view('template/header_admin', $lcPosition);
+                    $this->load->view('home/admins/content', $lcPosition);
+                    $this->load->view('template/footer_admin', $lcPosition);
+            }else{
+                redirect(base_url());
+            }
 
-                $lcPosition['data'] = $this->admin_model->getLecturerPosition();
-                $lcPosition['title'] = 'Position Lecturer';
-
-                $this->load->view('template/header_admin', $lcPosition);
-                $this->load->view('home/admins/content', $lcPosition);
-                $this->load->view('template/footer_admin', $lcPosition);
+             
             }
 
             public function dpaLecturer(){
-                //vu_dpa
-                $lcDPA['data'] = $this->admin_model->getLecturerDPA();
-                $lcDPA['title'] = 'DPA Lecturer';
+                if($this->session->userdata('loggedIn')){
+                    //vu_dpa
+                    $lcDPA['data'] = $this->admin_model->getLecturerDPA();
+                    $lcDPA['title'] = 'DPA Lecturer';
 
-                $this->load->view('template/header_admin', $lcDPA);
-                $this->load->view('home/admins/content', $lcDPA);
-                $this->load->view('template/footer_admin', $lcDPA);
+                    $this->load->view('template/header_admin', $lcDPA);
+                    $this->load->view('home/admins/content', $lcDPA);
+                    $this->load->view('template/footer_admin', $lcDPA);
+                }else{
+                    redirect(base_url());
+                }
+                
             }
 
             public function researchLecturer(){
-                //vu_research        
-                $lcResearch['data'] = $this->admin_model->getLecturerResearch();
-                $lcResearch['title'] = 'Research Group Lecturer';
+                if($this->session->userdata('loggedIn')){
+                    //vu_research        
+                    $lcResearch['data'] = $this->admin_model->getLecturerResearch();
+                    $lcResearch['title'] = 'Research Group Lecturer';
 
-                $this->load->view('template/header_admin', $lcResearch);
-                $this->load->view('home/admins/content', $lcResearch);
-                $this->load->view('template/footer_admin', $lcResearch);
+                    $this->load->view('template/header_admin', $lcResearch);
+                    $this->load->view('home/admins/content', $lcResearch);
+                    $this->load->view('template/footer_admin', $lcResearch);
+                }else{
+                    redirect(base_url());
+                }
+                
             }
 
             public function hourDistributionLecturer(){
+                if($this->session->userdata('loggedIn')){
                     //vu_hour_distribution
-                $lcHour['data'] = $this->admin_model->getHourDistribution();
-                $lcHour['title'] = 'Hour Dist.';
+                    $lcHour['data'] = $this->admin_model->getHourDistribution();
+                    $lcHour['title'] = 'Hour Dist';
 
-                $this->load->view('template/header_admin', $lcHour);
-                $this->load->view('home/admins/content', $lcHour);
-                $this->load->view('template/footer_admin', $lcHour);
+                    $this->load->view('template/header_admin', $lcHour);
+                    $this->load->view('home/admins/content', $lcHour);
+                    $this->load->view('template/footer_admin', $lcHour);
+                }else{
+                    redirect(base_url());
+                }
+                
             }
 
     //UPDATE
+        public function updateResearchGroup(){
+            if($this->session->userdata('loggedIn')){
+                $this->admin_model->updateResearchGroup();
+                redirect('admin_home/researchGroup');
+            }else{
+                redirect(base_url());
+            }
+            
+        }
+        public function updateClasses(){
+            if($this->session->userdata('loggedIn')){
+                $this->admin_model->updateClass();
+                redirect('admin_home/classList');
+            }else{
+                redirect(base_url());
+            }
+            
+        }
 
+        public function updateSubjects(){
+            if($this->session->userdata('loggedIn')){
+                $this->admin_model->updateSubject();
+                redirect('admin_home/subjectList');
+            }else{
+                redirect(base_url());
+            }
+           
+        }
+        public function updateStatusLecturer(){
+            if($this->session->userdata('loggedIn')){
+                
+            }else{
+                redirect(base_url());
+            }
+            
+        }
+        public function updateFieldLecturer(){
+            if($this->session->userdata('loggedIn')){
+                
+            }else{
+                redirect(base_url());
+            }
+            
+        }
+        public function updatePositionLecturer(){
+            if($this->session->userdata('loggedIn')){
+                
+            }else{
+                redirect(base_url());
+            }
+            
+        }
+        public function updateDPALecturer(){
+            if($this->session->userdata('loggedIn')){
+                
+            }else{
+                redirect(base_url());
+            }
+            
+        }
+        public function updateResearchGroupLecturer(){
+            if($this->session->userdata('loggedIn')){
+                
+            }else{
+                redirect(base_url());
+            }
+            
+        }
+        public function updateHourDist(){
+            if($this->session->userdata('loggedIn')){
+                
+            }else{
+                redirect(base_url());
+            }
+            
+        }
 
     //DELETE
+
+    public function deleteResearchGroup(){
+        if($this->session->userdata('loggedIn')){
+            $this->admin_model->deleteResearchGroup($this->input->post('rs_id'));
+            redirect('admin_home/researchGroup');
+        }else{
+            redirect(base_url());
+        }
+        
+    }
+    public function deleteClasses(){
+        if($this->session->userdata('loggedIn')){
+            $this->admin_model->deleteClass($this->input->post('cl_id'));
+            redirect('admin_home/classList');
+        }else{
+            redirect(base_url());
+        }
+        
+    }
+
+    public function deleteSubjects(){
+        if($this->session->userdata('loggedIn')){
+            $this->admin_model->deleteSubject($this->input->post('subject_code'));
+            redirect('admin_home/subjectList');    
+        }else{
+            redirect(base_url());
+        }
+        
+    }
+    /* public function deleteStatusLecturer(){
+        
+    }
+    public function deleteFieldLecturer(){
+        
+    } */
+    public function deletePositionLecturer(){
+        if($this->session->userdata('loggedIn')){
+                
+        }else{
+            redirect(base_url());
+        }
+        
+    }
+    public function deleteDPALecturer(){
+        if($this->session->userdata('loggedIn')){
+                
+        }else{
+            redirect(base_url());
+        }
+        
+    }
+    public function deleteResearchGroupLecturer(){
+        if($this->session->userdata('loggedIn')){
+                
+        }else{
+            redirect(base_url());
+        }
+        
+    }
+    public function deleteHourDist(){
+        if($this->session->userdata('loggedIn')){
+                
+        }else{
+            redirect(base_url());
+        }
+        
+    }
 }
 
 /* End of file home.php */
